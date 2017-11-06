759c536beaf2b296d56c92fafe81b286ac1c04a6
louner <lounera@gmail.com>
2017-11-06 23:39:29 +0800

    add step

--- cnn/validate.py
+++ cnn/validate.py
@@ -2,11 +2,14 @@ from train import *
 from sklearn.cross_validation import KFold
 from tensorflow.python.framework import ops
 
+df = df.sample(frac=1)
 kf = KFold(df.shape[0], n_folds=10, shuffle=True)
-epoch_number = 2
+epoch_number = 1
 i = 0
+print(df.shape)
 
 logger.info('start validation')
+step = 0
 for train_index, test_index in kf:
     train_set = df.iloc[train_index, :]
     test_set = df.iloc[test_index, :]
@@ -16,5 +19,6 @@ for train_index, test_index in kf:
     with tf.Session() as sess:
         graphs = build_graph(sess)
 
-        train(train_set, './model/validate', epoch_number, graphs, sess)
-        test(test_set, './model/validate', graphs, sess)
+        train(train_set, './models/validate', epoch_number, graphs, sess)
+        test(test_set, './models/validate', graphs, sess, str(step))
+        step += 1
